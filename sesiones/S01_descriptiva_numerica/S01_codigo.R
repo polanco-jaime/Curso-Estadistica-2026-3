0665f39c3b6216d3b2cae78f01bcbcab4445b0b3
polanco-jaime <japj182@hotmail.com>
2026-07-27 16:54:39 -0500

    Auto-sync: actualización 2026-07-27 16:54

--- sesiones/S01_descriptiva_numerica/S01_codigo.R
+++ sesiones/S01_descriptiva_numerica/S01_codigo.R
@@ -6,8 +6,11 @@
 
 # --- Cargar paquetes y datos -------------------------------------------------
 library(tidyverse)
+library(arrow)
 
-# datos <- read.csv("../../datos/saber_pro/ARCHIVO.csv")
+gh_raw <- "https://raw.githubusercontent.com/polanco-jaime/Curso-Estadistica-2026-3/main/datos"
+saber_pro <- read_parquet(paste0(gh_raw, "/saber_pro/saber_pro.parquet"))
+saber11   <- read_parquet(paste0(gh_raw, "/saber11/saber11.parquet"))
 
 # --- Medidas de tendencia central --------------------------------------------
 
